872a51654622709bb6abc12cfb81317c82262acf
Alexander Tihonruk <a.tihonruk@gmail.com>
2013-09-12 21:34:26 +0300

    Updated project definition.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/java.jdbc "0.3.0-alpha4"]
                  [org.apache.poi/poi "3.9"]
                  [org.apache.poi/poi-ooxml "3.9"]]
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :main clj-xlstream.core)
